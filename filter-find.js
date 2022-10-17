7cc017b040cb91efeb2a92554e752b21946c51bf
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-10-17 20:51:21 +0600

    filter find

---
+++ filter-find.js
@@ -0,0 +1,14 @@
+const numbers = [5, 13, 7, 41, 30, 5, 2, 19];
+const bigNumbers = numbers.filter(number => number > 20);
+const smallNumbers = numbers.filter(number => number < 10);
+// console.log( smallNumbers);
+
+const products = [
+    { name: 'water bottle', price: 50, color: 'yellow' },
+    { name: 'mobile phone', price: 15000, color: 'black' },
+    { name: 'Smart watch', price: 3000, color: 'black' },
+    { name: 'stick note', price: 30, color: 'pink' },
+    { name: 'water glass', price: 3, color: 'white' },
+];
+
+const expensive = products.filter(product =>)
--- more-map.js
+++ more-map.js
@@ -16,5 +16,6 @@ const productsName = products.map(product => product.phone);
 const productsprice = products.map(product => product.price);
 const productscolor = products.map(product => product.color);
 // products.map(product => console.log(product));
+
 products.forEach(product => console.log(product)) //return korano proyojon na hola forEach babohar kora jai
 console.log(productscolor);
